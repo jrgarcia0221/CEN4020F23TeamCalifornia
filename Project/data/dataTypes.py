dba638f98395bf252f7a9a399b98fe290c6671b3
grant <debiase1@usf.edu>
2023-10-03 09:07:38 -0400

    Added data folder with dataTypes.py and jsonwrapper.py

---
+++ Project/data/dataTypes.py
@@ -0,0 +1,35 @@
+import json
+
+#Author Grant DeBiase
+#Function to create setting dictionary
+def createSetting(email="On", sms="On",targetedAdvert="On", language="English"):
+    return {
+        "email": email,
+        "sms": sms,
+        "targetedAdvert": targetedAdvert,
+        "language":language
+    }    
+
+#Author Grant DeBiase
+#Function to create student dictionary
+def createStudent(username="JohnDoe", password="Password123!", firstname="John", lastname="Doe", settings=createSetting()):
+    return {
+        "username": username,
+        "password": password,
+        "firstname": firstname,
+        "lastname": lastname,
+        "settings": settings
+    }
+    
+#Author Grant DeBiase
+#Function to create job dictionary
+def createJob(title="title", description="description", employer="employer", location="location", salary="salary", firstname="John", lastname="Doe"):
+    return {
+        "title": title,
+        "description": description,
+        "employer": employer,
+        "location": location,
+        "salary": salary,
+        "firstname": firstname,
+        "lastname": lastname
+    }
---
+++ Project/data/jsonWrapper.py
@@ -0,0 +1,23 @@
+import json
+
+#Author Grant DeBiase
+#converts object to json string
+def serialize(obj):
+    return json.dumps(obj, indent=4)
+
+#Author Grant DeBiase
+#writes json string of object to file
+def serializeToFile(obj, fileName):
+    with open(fileName, 'w') as file:
+        json.dump(obj, file, indent=4)
+
+#Author Grant DeBiase
+#converts json string to object
+def deserialize(jsonStr):
+    return json.loads(jsonStr)
+
+#Author Grant DeBiase
+#reads json string from file and converts it to object
+def deserializeFromFile(fileName):
+    with open(fileName, 'r') as file:
+        return json.load(file)
